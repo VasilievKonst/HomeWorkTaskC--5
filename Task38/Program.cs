8e7607af9d0428641145cca9703aeed02a566673
KonstantinVasiliev <grafit21@mail.ru>
2022-11-05 10:15:30 +0500

    Save task 38

--- Task38/Program.cs
+++ Task38/Program.cs
@@ -1,2 +1,68 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿double[] array = new double[6];
+
+void FullArray(double[] collect)
+{
+    int lenght = collect.Length;
+    int index = 0;
+    while(index < lenght)
+    {
+        collect[index] = new Random().Next(-100,100);
+        index++;
+    }
+}
+
+void PrintArray(double[] col)
+{
+    int count = col.Length;
+    int position = 0;
+    while(position < count)
+    {
+        Console.WriteLine(col[position]);
+        position++;
+    }
+}
+
+double SearchMAX(double[] ar)
+{
+    int countAR = ar.Length;
+    int ind = 0;
+    double  max = ar[ind];
+    while(ind < countAR)
+    {
+        if(ar[ind] > max)
+        {
+            max = ar[ind];
+        }
+        ind++;
+    }
+    return max;
+}
+
+double SearchMIN(double[] arm)
+{
+    int countARR = arm.Length;
+    int inde = 0;
+    double min = arm[inde];
+    while(inde < countARR)
+    {
+        if(arm[inde] < min)
+        {
+            min = arm[inde];
+        }
+        inde++;
+    }
+    return min;
+}
+void PrintResault()
+{
+    double resaultsum = Math.Abs(SearchMIN(array)) - Math.Abs(SearchMAX(array));
+   
+    Console.Write("Разность между максимальным и минимальным элементом массива составляет: ");
+    Console.WriteLine(resaultsum);
+}
+
+Console.WriteLine("Задан массив: ");
+
+FullArray(array);
+PrintArray(array);
+PrintResault();
